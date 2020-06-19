2cb062e335757b8556c24a7e970b56f8aa81ad4b
junow <taristmas@gmail.com>
2020-06-19 18:28:26 +0900

    1092 주석추가

--- problems/boj/1092/junow.cpp
+++ problems/boj/1092/junow.cpp
@@ -23,6 +23,7 @@ int main(void) {
   int i = 0, j = 0;
   int ans = 0;
   if (a[0] < b[0]) {
+    // 제일 무거운 화물이 제일 쎈 크레인보다 무거우면 못 옮김
     cout << "-1\n";
     return 0;
   }
@@ -30,30 +31,23 @@ int main(void) {
   int cnt = 0;
   while (1) {
     if (cnt >= m) {
+      // 전체 다 옮겼으면 그만둠
       break;
     }
     int i = 0;
     int j = 0;
+
     while (i < n && j < m) {
       if (a[i] >= b[j]) {
-        i++;
-        j++;
+        // i 번째 크레인이 j 번쨰 화물 옮길 수 있는 경우
+        i++;  // 다음 크레인
+        j++;  // 다음 화물
         cnt++;
       } else {
+        // 못 옮기면 다음 화물 확인
         j++;
       }
-
-      if (cnt == m) {
-        break;
-      }
-
-      if (i == n) {
-        i = 0;
-        j = 0;
-        ans++;
-      }
     }
-
     ans++;
   }
 
