8c3f67dba27729e080f9077d736bb0099d194d0d
brillante06 <marshmello37@naver.com>
2020-06-22 17:07:23 +0900

    Brillante06/boj/1543 (#6)

---
+++ problems/boj/1543/brillante06.py
@@ -0,0 +1,14 @@
+from sys import stdin
+# 1543
+strVal = input()
+findVal = input()
+
+# print(findVal,strVal)
+i, start, res = 0, 0, 0
+while i <= len(strVal)-len(findVal):
+    if strVal[i:i+len(findVal)] == findVal:
+        res += 1
+        i += len(findVal)
+    else:
+        i += 1
+print(res)
