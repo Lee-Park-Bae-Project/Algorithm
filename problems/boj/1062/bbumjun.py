8d0eb3a92b73b58a12a9fe02d3a1612ccbf32011
bbumjun <bumjun7928@naver.com>
2020-06-23 19:02:39 +0900

    bbumjun boj 1062 가르침

--- problems/boj/1062/bbumjun.py
+++ problems/boj/1062/bbumjun.py
@@ -10,18 +10,18 @@ for i in range(n):
         if c not in defaultC:
             word.add(c)
     words.append(word)
-if k < 5:
+if k < 5:  # 기본 5개 문자열 가르칠 수 있는지
     print(0)
     sys.exit()
 words.sort(key=lambda words: len(words))
-candidates = set([])
+candidates = set([])  # 가르칠 수 있는 글자 후보
 for word in words:
     for c in word:
         candidates.add(c)
 cb = combinations(candidates, k-5 if k-5 <
                   len(candidates) else len(candidates))
 ans = 0
-for x in cb:
+for x in cb:  # 글자 후보의 조합
     res = 0
     for word in words:
         for c in word:
