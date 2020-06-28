d7f8ae7398e9ff49841e8081db4cb684273457ce
junow <taristmas@gmail.com>
2020-06-29 01:46:24 +0900

    Modified junow/boj/1062

--- problems/boj/1062/junow.cpp
+++ problems/boj/1062/junow.cpp
@@ -5,7 +5,33 @@ int n, k, ans;
 vector<string> check;
 bool selected[26];
 
-vector<int> bm;
+void dfs(int _i, int cnt) {
+  if (cnt == k - 5) {
+    int candi = 0;
+    for (int i = 0; i < n; i++) {
+      bool flag = true;
+      for (int j = 0; j < check[i].size(); j++) {
+        if (!selected[check[i][j] - 'a']) {
+          flag = false;
+          break;
+        }
+      }
+      if (flag) {
+        candi++;
+      }
+    }
+
+    ans = max(ans, candi);
+    return;
+  }
+
+  for (int i = _i; i < 26; i++) {
+    if (selected[i]) continue;
+    selected[i] = 1;
+    dfs(i, cnt + 1);
+    selected[i] = 0;
+  }
+}
 
 int main(void) {
   ios_base::sync_with_stdio(false);
@@ -21,15 +47,24 @@ int main(void) {
     return 0;
   }
   string s;
-  bm.resize(n);
 
   for (int i = 0; i < n; i++) {
     cin >> s;
     int len = s.size();
-    for (int j = 0; j < len; j++) {
-      bm[i] |= (1 << (s[j] - 'a'));
+    string t = "";
+    for (int j = 4; j < len - 4; j++) {
+      t += s[j];
     }
+    check.push_back(t);
   }
+
+  selected[0] = true;
+  selected[2] = true;
+  selected[8] = true;
+  selected[13] = true;
+  selected[19] = true;
+
+  dfs(0, 0);
 
   cout << ans << "\n";
 
