a8bebd15c7c3ccb13a88ec9e819d0f4a43120486
bbumjun <bumjun7928@naver.com>
2020-06-29 20:59:16 +0900

    bbumjun boj 2110 공유기 설치

---
+++ problems/boj/2110/bbumjun.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+homes = []
+for i in range(n):
+    homes.append(int(input()))
+homes.sort()
+minGap = homes[1]-homes[0]
+maxGap = homes[-1]-homes[0]
+ans = 0
+if m < 3:
+    ans = maxGap
+else:
+    while minGap <= maxGap:
+        middle = (minGap + maxGap) // 2
+        cnt = m-2
+        point = homes[0]
+        for i in range(1, len(homes)-1):
+            if homes[i] - point >= middle and homes[-1] - homes[i] >= middle:
+                cnt -= 1
+                point = homes[i]
+            if cnt <= 0:
+                ans = middle
+                minGap = middle+1
+                break
+        else:
+            maxGap = middle - 1
+print(ans)
